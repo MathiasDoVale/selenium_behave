52ab0ef5b6d8a65aaa9c33f7e046192360e4d334
Mathido <matdovale@gmail.com>
2023-04-18 22:38:57 -0300

    Background steps

---
+++ features/steps/backgroundsteps.py
@@ -0,0 +1,20 @@
+from behave import *
+
+@when(u'navigate to search page')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@then(u'search page should display')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@when(u'navigate to advance search user')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@then(u'advanced search page should display')
+def step_impl(context):
+    assert True, "Test passed"
